d8ab254136d8c9ce91d75336ac3841b9c61740ac
sobamentuyu <sohgo.kobatyan@icloud.com>
2025-05-21 19:34:09 +0900

    todolist作成中

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,40 @@
 import { useState } from "react";
-import "./App.css";
+import './App.css';
+import TodoList from './Todolist.tsx'
 
 function App() {
-  const [tasks, setTasks] = useState<string[]>([]);
-  const [taskInput, setTaskInput] = useState<string>("");
-  const [fishCount, setFishCount] = useState<number>(0);
 
-  const addTask = () => {
-    if (taskInput.trim() !== "") {
-      setTasks([...tasks, taskInput.trim()]); // 新しいタスクを追加
-      setFishCount(fishCount + 1); // 魚の数を増やす
-      setTaskInput(""); // 入力欄をクリア
-    } else {
-      alert("タスクを入力してください！");
-    }
-  };
+  const [todos, setTodos] = useState([
+    {id: 1, name: 'todo1', completed: false}
+  ])
+  //const [tasks, setTasks] = useState<string[]>([]);
+  //const [taskInput, setTaskInput] = useState<string>("");
+  //const [fishCount, setFishCount] = useState<number>(0);
 
-  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === "Enter") {
-      addTask();
-    }
-  };
+  //const addTask = () => {
+  //  if (taskInput.trim() !== "") {
+  //    setTasks([...tasks, taskInput.trim()]); // 新しいタスクを追加
+  //    setFishCount(fishCount + 1); // 魚の数を増やす
+  //    setTaskInput(""); // 入力欄をクリア
+  //  } else {
+  //    alert("タスクを入力してください！");
+  //  }
+  //};
+
+  //const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+  //  if (e.key === "Enter") {
+  //    addTask();
+  //  }
+  //};
 
   return (
-    <div className="container">
+    <div>
       <h1>おさかなToDoリスト</h1>
-
-      <div className="todo-input-area">
-        <input
-          type="text"
-          id="taskInput"
-          placeholder="新しいタスクを入力..."
-          value={taskInput}
-          onChange={(e) => setTaskInput(e.target.value)}
-          onKeyPress={handleKeyPress}
-        />
-        <button id="addTaskButton" onClick={addTask}>
-          タスクを追加
-        </button>
-      </div>
-
-      <div className="todo-list-area">
-        <h2>今日のタスク</h2>
-        <ul id="taskList">
-          {tasks.map((task, index) => (
-            <li key={index}>{task}</li> // 各タスクにユニークなkeyを設定
-          ))}
-        </ul>
-      </div>
-
-      <div className="fish-tank-area">
-        <h2>私の魚たち</h2>
-        <div id="fishTank">
-          {Array.from({ length: fishCount }).map((_, index) => (
-            <div key={index} className="fish"></div>
-          ))}
-        </div>
-      </div>
+      <p>残りのおさかな：0</p>
+      <input />
+      <button>おさかなを増やす</button>
+      <button>おさかなを食べる</button>
+      <TodoList todos={todos} />
     </div>
   );
 }
