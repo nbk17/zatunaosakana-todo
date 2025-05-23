a21287076f98aae17716cd7e098bdfe717d97990
sobamentuyu <sohgo.kobatyan@icloud.com>
2025-05-23 19:16:38 +0900

    微修正

--- src/components/RandomImage.tsx
+++ src/components/RandomImage.tsx
@@ -2,13 +2,18 @@ import {useEffect, useState} from "react";
 
 //魚インポートここから
 import fish1 from '../assets/ナンヨウハギモドキ.png';
+import fish2 from '../assets/えびかにっくすで！.png';
+import fish3 from '../assets/おれんじさかな.png';
+import fish4 from '../assets/きめーいか.png';
+import fish5 from '../assets/ノット雑フグ.png';
+import fish6 from '../assets/死んだ目魚.png';
 //魚インポートここまで
 
 //以下関数
 function RandomImage(){
 
     const images=[
-        fish1,//魚追加
+        fish1,fish2,fish3,fish4,fish5,fish6//魚追加
     ];
     const [randomImage,setRandomImage]=useState('');
 
